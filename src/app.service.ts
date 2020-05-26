51735757151b3344eb0bd17f761c3365809deb77
Jonas Wallmann <jonaswallmann@hotmail.de>
2020-05-26 16:07:05 +0200

    added simulated delay feature

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,19 +2,19 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  private success: boolean = false;
 
-  getMsg(): String {
-    if (this.success) {
-      return "I'm alright.";
-    } else {
-      throw new HttpException(
-        {
-          status: 500,
-          error: "I'm not alright.",
-        },
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+  private success: boolean = true;
+  private delay = 5000;
+
+  async getMsg(): Promise<String> {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        if (this.success) {
+          res("I'm alright.");
+        } else {
+          rej();
+        }
+      }, this.delay)
+    })
   }
 }
